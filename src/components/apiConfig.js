20ce34ddae778071db108a9488ef7a3e7ad8d1c5
ithub.south@leadschool.in <ithub.south@leadschool.in>
2025-11-05 15:29:32 +0530

    V4.1.4 Published

---
+++ src/components/apiConfig.js
@@ -0,0 +1,10 @@
+// apiConfig.js
+export const SOA_API_CONFIG = {
+  url: 'https://apigw.leadschool.in/soa/admin/soa',
+  apiKey: 'g6tviMWCTkRmDeFidrcvkcQ97ldcNdhI'
+};
+
+export const OMS_API_CONFIG = {
+  url: 'https://apigw.leadschool.in/oms/warehouse/orders/devices',
+  apiKey: 'f6kA3UF8?9GV'
+};
